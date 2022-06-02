a14ddc84c746a0f2734ac9cc8b3d86b166a928a1
Ceyhun <Ceyhun@DESKTOP-KRB1MHC>
2022-06-02 04:38:19 +0300

    Add project files.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        [StringLength(50)]
+        public string Category { get; set; }
+        public bool IsComplete { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
---
+++ Services/TodoService.cs
@@ -0,0 +1,13 @@
+﻿using Todo.Data;
+namespace Todo.Services
+{
+    public class TodoService
+    {
+        protected readonly TodoDbContext _dbcontext;
+
+        public TodoService(TodoDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+    }
+}
